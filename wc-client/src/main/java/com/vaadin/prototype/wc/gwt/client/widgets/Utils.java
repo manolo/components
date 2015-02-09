c67936b27e25cdd1f5ae8fe1d39813457a10e4cb
Manolo Carrasco <manolo@vaadin.com>
2015-02-09 10:00:30 +0100

    Renaming namespace in Utils class
    Change-Id: I903f41ce9fa3481b6f2ad4ba910c507536904d54

--- wc-client/src/main/java/com/vaadin/prototype/wc/gwt/client/widgets/Utils.java
+++ wc-client/src/main/java/com/vaadin/prototype/wc/gwt/client/widgets/Utils.java
@@ -3,8 +3,12 @@ package com.vaadin.prototype.wc.gwt.client.widgets;
 import com.google.gwt.core.client.js.JsExport;
 import com.google.gwt.i18n.client.NumberFormat;
 
-@JsExport
-public class WCFormat {
+/**
+ * All methods here will be exported as window.vaadin.Utils, example:
+ * window.vaadin.Utils.formatCurrency(new Date().getTime())
+ */
+@JsExport()
+public class Utils {
     @JsExport
     public static String formatCurrency(double val) {
         NumberFormat fmt = NumberFormat.getCurrencyFormat();
